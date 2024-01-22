79571ecd272a4585e05b3d22f5fc109ec5ae69bc
ibraztech2 <ibraztech2@gmail.com>
2024-01-22 11:45:23 +0100

    modifying feature and additional feature

--- integrator.py
+++ integrator.py
@@ -3,17 +3,17 @@ print('|______________________________________________|')
 print('On this platform you can do various operation \n______________________________________________\n1.playing tic tac toe game[1] \n______________________________________________\n2. play random_word_Choose game[2] \n______________________________________________\n3. Temperature converter[3] \n______________________________________________ \nEnter the numoer in front of each to choose one  ')
 print('______________________________________________')
 ask = input("what do you want to do ? ").strip().lower()
-no_of_game_played = 0
+play_Agin = True
 from tic_tac_toe_project import play
 if ask == '1':
-    no_of_game_played += 1
-    play()
-    play_again = input("Do you want to play again ? (Yes/No ?").strip().upper()
-    while play_again == "YES":
-        no_of_game_played += 1
+    while play_Agin:
         play()
+        play_again = input("Do you want to play again ? (Yes/No ?").strip().upper()
+        if play_again == 'YES':
+            play_Agin = True
+        else:
+            play_Agin = False
     else:
-        print('You ve played '+str(no_of_game_played)+ ' times')
         print('Bye')
 elif ask == "2":
     pass
